d138612e4692aa3f97563e2b3c0b6a9cbb8043b9
jotafab <fabrizioore512@gmail.com>
2025-10-31 15:44:40 -0500

    asdfas

--- modules/home/wofi.nix
+++ modules/home/wofi.nix
@@ -3,42 +3,64 @@
 {
   programs.wofi = {
     enable = true;
+    settings = {
+      content_halign = "fill";
+      insensitive = true;
+      allow_images = true;
+      image_size = 25;
+      term = "ghostty";
+    };
     style = ''
+      window {
+      margin: 0px;
+      border: 1px solid #005bf8ff;
+      background-color: #282a36;
+      }
+
       #input {
       margin: 5px;
       border: none;
-      color: #d8dee9;
-      background-color: #1e1e1e;
+      color: #f8f8f2;
+      background-color: #44475a;
       }
 
       #inner-box {
       margin: 5px;
       border: none;
-      background-color: #1e1e1e; 
+      background-color: #282a36;
       }
 
       #outer-box {
       margin: 5px;
       border: none;
-      background-color: #1e1e1e;
+      background-color: #282a36;
       }
 
       #scroll {
       margin: 0px;
-      border: 1e1e1e;
+      border: none;
       }
 
       #text {
       margin: 5px;
       border: none;
-      color: #c4c4c4;
+      color: #f8f8f2;
+      } 
+
+      #entry.activatable #text {
+      color: #282a36;
+      }
+
+      #entry > * {
+      color: #f8f8f2;
       }
 
       #entry:selected {
-      background-color: #1e1e1e;
+      background-color: #44475a;
       }
-      window {
-          border-radius: 2px; /* 10px radius for rounded corners */
+
+      #entry:selected #text {
+      font-weight: bold;
       }
        
     '';
