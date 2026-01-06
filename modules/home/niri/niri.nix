e2ec9e3b3056fc33d508ab5c7e1d951bbfdebff8
jotafab <fabrizioore512@gmail.com>
2026-01-06 07:38:40 -0500

    feat: update swaylock configuration and add swaylock.conf file

--- modules/home/niri/niri.nix
+++ modules/home/niri/niri.nix
@@ -2,7 +2,7 @@
 
 {
 	xdg.configFile."niri/config.kdl".source = ./config.kdl;
-	xdg.configFile."swaylock/config".source = ./mocha.conf;
+	xdg.configFile."swaylock/config".source = ./swaylock.conf;
   home.file.".local/bin/nws.sh".source = ./nws.sh;
   
 }
